d8a12a92753c366997ab75784da723c07a6934a4
Liangyiu <ms290498@gmail.com>
2025-06-21 00:25:22 +0200

    added react hook form + zod validation

--- claimly/app/(auth)/login.tsx
+++ claimly/app/(auth)/login.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import {
   Card,
@@ -12,32 +19,44 @@ import { Button } from "~/components/ui/button";
 import { Link } from "expo-router";
 import { useRouter } from "expo-router";
 import { authClient } from "~/lib/auth-client";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(124, "Password must be less than 124 characters"),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
 
 type Props = {};
 
 const Login = (props: Props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { control, handleSubmit, trigger, formState } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+  });
+
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const router = useRouter();
 
-  const handleLogin = async () => {
-    if (!email || !password) {
-      setError("All fields are required");
-      return;
-    }
-    setError("");
-    setIsLoading(true);
+  const handleLogin = async ({ email, password }: LoginSchema) => {
+    setLoading(true);
+
     await authClient.signIn.email(
       { email, password },
       {
         onError: (error) => {
-          setIsLoading(false);
+          setLoading(false);
           setError(error.error.message);
         },
         onSuccess: () => {
-          setIsLoading(false);
+          setLoading(false);
           router.replace("/shifts");
         },
       }
@@ -45,53 +64,80 @@ const Login = (props: Props) => {
   };
 
   return (
-    <View className="flex-1 gap-5 justify-center items-center p-6 bg-secondary/30">
-      <Card className="p-6 w-full max-w-sm rounded-2xl">
-        <CardHeader className="items-center">
-          <CardTitle className="pb-2 text-center">Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <View className="p-3" />
-          <View className="flex-col gap-3">
-            <Input
-              placeholder="Email"
-              keyboardType="email-address"
-              value={email}
-              onChangeText={setEmail}
-            />
-            <Input
-              placeholder="Password"
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-            />
-          </View>
-
-          {error && (
-            <View className="flex items-center w-full">
-              <Text className="text-center text-red-500">{error}</Text>
-            </View>
-          )}
-
-          <View className="p-4" />
-          <Button className="w-full" onPress={handleLogin}>
-            {isLoading ? (
-              <ActivityIndicator />
+    <View className="flex-1 p-6 bg-secondary/30">
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        className="flex-1 justify-center"
+      >
+        <View className="gap-4 self-center w-full max-w-sm">
+          <Text className="pb-4 text-3xl font-bold text-center text-foreground">
+            Login
+          </Text>
+          <Controller
+            control={control}
+            name="email"
+            render={({
+              field: { onChange, value, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  placeholder="Email"
+                  keyboardType="email-address"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                />
+                {error && <Text className="text-red-500">{error.message}</Text>}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({
+              field: { onChange, value, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  placeholder="Password"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                />
+                {error && <Text className="text-red-500">{error.message}</Text>}
+              </>
+            )}
+          />
+          {error && <Text className="text-red-500">{error}</Text>}
+          <Button
+            className="mt-4 w-full"
+            onPress={handleSubmit(handleLogin)}
+            disabled={!formState.isValid || loading}
+          >
+            {loading ? (
+              <ActivityIndicator className="text-foreground" />
             ) : (
               <Text className="text-center">Login</Text>
             )}
           </Button>
-        </CardContent>
-        <CardFooter>
-          <View className="flex items-center w-full">
-            <Link href="/register">
-              <Text className="text-center underline color-foreground">
-                Register instead?
-              </Text>
-            </Link>
-          </View>
-        </CardFooter>
-      </Card>
+        </View>
+      </KeyboardAvoidingView>
+      <View className="items-center pb-4">
+        <Button
+          variant="link"
+          onPress={() => router.push("/register")}
+          className="p-0"
+        >
+          <Text className="text-center underline color-foreground">
+            Register instead?
+          </Text>
+        </Button>
+      </View>
     </View>
   );
 };
--- claimly/app/(auth)/register.tsx
+++ claimly/app/(auth)/register.tsx
@@ -11,42 +11,46 @@ import { Link, useRouter } from "expo-router";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { authClient } from "~/lib/auth-client";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { KeyboardAvoidingView } from "react-native";
+import { Platform } from "react-native";
+
+const registerSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(124, "Password must be less than 124 characters"),
+});
+
+type RegisterSchema = z.infer<typeof registerSchema>;
 
 type Props = {};
 
 const Register = (props: Props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
   const router = useRouter();
+  const { control, handleSubmit, formState } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onChange",
+  });
 
-  const handleRegister = async () => {
-    if (!email || !password || !name) {
-      setError("All fields are required");
-      return;
-    }
-
-    if (password.length < 8) {
-      setError("Password must be at least 8 characters long");
-      return;
-    }
-
-    setError("");
-
-    setIsLoading(true);
+  const handleRegister = async ({ email, password, name }: RegisterSchema) => {
+    setLoading(true);
 
     await authClient.signUp.email(
-      { email, password, name: email },
+      { email, password, name },
       {
         onError: (error) => {
-          setIsLoading(false);
+          setLoading(false);
           setError(error.error.message);
         },
         onSuccess: () => {
-          setIsLoading(false);
+          setLoading(false);
           router.replace("/");
         },
       }
@@ -54,54 +58,99 @@ const Register = (props: Props) => {
   };
 
   return (
-    <View className="flex-1 gap-5 justify-center items-center p-6 bg-secondary/30">
-      <Card className="p-6 w-full max-w-sm rounded-2xl">
-        <CardHeader className="items-center">
-          <CardTitle className="pb-2 text-center">Register</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <View className="p-3" />
-          <View className="flex-col gap-3">
-            <Input placeholder="Name" value={name} onChangeText={setName} />
-            <Input
-              placeholder="Email"
-              keyboardType="email-address"
-              value={email}
-              onChangeText={setEmail}
-            />
-            <Input
-              placeholder="Password"
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-            />
-          </View>
-
-          {error && (
-            <View className="flex items-center w-full">
-              <Text className="text-center text-red-500">{error}</Text>
-            </View>
-          )}
-
-          <View className="p-4" />
-          <Button className="w-full" onPress={handleRegister}>
-            {isLoading ? (
-              <ActivityIndicator />
+    <View className="flex-1 p-6 bg-secondary/30">
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        className="flex-1 justify-center"
+      >
+        <View className="gap-4 self-center w-full max-w-sm">
+          <Text className="pb-4 text-3xl font-bold text-center text-foreground">
+            Register
+          </Text>
+          <Controller
+            control={control}
+            name="name"
+            render={({
+              field: { onChange, value, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  placeholder="Name"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="words"
+                />
+                {error && <Text className="text-red-500">{error.message}</Text>}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({
+              field: { onChange, value, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  placeholder="Email"
+                  keyboardType="email-address"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                />
+                {error && <Text className="text-red-500">{error.message}</Text>}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({
+              field: { onChange, value, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <Input
+                  placeholder="Password"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                />
+                {error && <Text className="text-red-500">{error.message}</Text>}
+              </>
+            )}
+          />
+          {error && <Text className="text-red-500">{error}</Text>}
+          <Button
+            className="mt-4 w-full"
+            onPress={handleSubmit(handleRegister)}
+            disabled={!formState.isValid || loading}
+          >
+            {loading ? (
+              <ActivityIndicator className="text-foreground" />
             ) : (
               <Text className="text-center">Register</Text>
             )}
           </Button>
-        </CardContent>
-        <CardFooter>
-          <View className="flex items-center w-full">
-            <Link href="/login">
-              <Text className="text-center underline color-foreground">
-                Login instead?
-              </Text>
-            </Link>
-          </View>
-        </CardFooter>
-      </Card>
+        </View>
+      </KeyboardAvoidingView>
+      <View className="items-center pb-4">
+        <Button
+          variant="link"
+          onPress={() => router.push("/login")}
+          className="p-0"
+        >
+          <Text className="text-center underline color-foreground">
+            Login instead?
+          </Text>
+        </Button>
+      </View>
     </View>
   );
 };
